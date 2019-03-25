41f315edcf97d16a37e77ca724da1f6388266d5f
XeniaOlshanskaya <ks14012000@gmail.com>
2019-03-25 22:00:27 +0700

    Окончательная версия

--- filesystem.py
+++ filesystem.py
@@ -73,3 +73,67 @@ def findFiles(target, path):
     except PermissionError:
         pass
     return new_list
+
+
+def acceptCommand():
+    command = int(input('Введите номер команды, которую хотите выполнить: '))
+    while command >= 8 and command <= 0:
+        command = input('Номер команды введен некорректно, повторите попытку, пожалуйста: ')
+    return command
+
+
+def runCommand(command):
+    if command == 1:
+        showCatalog()
+    elif command == 2:
+        moveUp()
+    elif command == 3:
+        moveDown()
+    elif command == 4:
+        path = os.getcwd()
+        print('В данной директории находится', countFiles(path), 'файлов и каталогов')
+    elif command == 5:
+        path = os.getcwd()
+        print('Размер текущего каталога', countBytes(path), 'байтов')
+    elif command == 6:
+        path = os.getcwd()
+        target = input('Введите имя файла, который хотите найти: ')
+        a = findFiles(target, path)
+        if len(a) == 0:
+            print('Данный файл найден не был.')
+        else:
+            a = findFiles(target, path)
+            a = str(a)
+            a = a.replace("[", "")
+            a = a.replace("]", "")
+            a = a.replace(" ", "")
+            a = a.replace(",", "")
+            a = a.replace("'", " ' ")
+            print(a)
+    else:
+        return
+    return
+
+
+def main():
+    command = 0
+    menu = '1. Просмотр каталога \n' \
+           '2. На уровень вверх \n' \
+           '3. На уровень вниз \n' \
+           '4. Количество файлов и каталогов \n' \
+           '5. Размер текущего каталога (в байтах) \n' \
+           '6. Поиск файла \n' \
+           '7. Выход из программы'
+    while command != 7:
+        print(os.getcwd())
+        print(menu)
+        command = acceptCommand()
+        if command == 7:
+            print('Работа программы завершена.')
+            break
+        else:
+            runCommand(command)
+
+
+if __name__ == "__main__":
+    main()
